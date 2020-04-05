73977e98323b11c0f5853256587366b35db30c61
root <root@instance-1.us-central1-a.c.zeta-handler-268716.internal>
2020-04-05 00:45:19 +0000

    Add lineage.mk

---
+++ lineage.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This files???
+#$(call inherit-product, $(LOCAL_PATH)/pre-base.mk)
+#$(call inherit-product-if-exists, vendor/google/gapps.mk)
+
+#TODO: Add this file
+#$(call inherit-product, $(LOCAL_PATH)/full_vky.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+#RR_BUILDTYPE = RevTechs.me
+
+PRODUCT_NAME := lineage_vtr
+PRODUCT_DEVICE := vtr
+PRODUCT_BRAND := Huawei
+PRODUCT_MODEL := P10
+
+# TODO: Update this
+# Override device name
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=kirin960
+
+#previus: kirin970
